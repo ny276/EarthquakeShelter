385a93c90c247bd8dce532847f89b4763d427cf3
D7703_17 <d7703_17@d7703-17ui-imac.local>
2018-10-15 12:17:10 +0900

    현재 위치추적

--- EarthquakeShelter/MapViewController.swift
+++ EarthquakeShelter/MapViewController.swift
@@ -8,13 +8,16 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class MapViewController: UIViewController {
       
       @IBOutlet weak var mapView: MKMapView!
       @IBOutlet weak var segment: UISegmentedControl!
       
-      var locationManager = ""
+      var locationManager = CLLocationManager()
+      var myViewController = MapViewController()
+      
       override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -50,5 +53,36 @@ class MapViewController: UIViewController {
 //                  //DustMapView.selectAnnotation(minDistAnno!, animated: true)
 //            }
 //      }
+      }
+       // 초기 맵 region 설정
+      func zoomToRegion() {
+            
+            print("zoom to Location")
+            // 부산지도 구글 중심정 35.163246, 129.066297
+            let location = CLLocationCoordinate2D(latitude: 35.118002, longitude: 129.121017)
+            let span = MKCoordinateSpan(latitudeDelta: 0.27, longitudeDelta: 0.27)
+            let region = MKCoordinateRegion(center: location, span: span)
+            mapView.setRegion(region, animated: true)
+            
+            // anno test
+            // add annotaton
+            //        let annotation = MKPointAnnotation()
+            //        annotation.coordinate = location
+            //        annotation.title = "동의과학대학교"
+            //        annotation.subtitle = "DIT"
+            //        DustMapView.addAnnotation(annotation)
+            //        DustMapView.delegate = self
+      }
+       // 현재 위치정보 트랙킹(currnet location tracking)
+      func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+            let userLocation: CLLocation = locations[0]
+            
+            let center = CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude)
+            print("center = \(center)")
+            
+            let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.21, longitudeDelta: 0.21))
+            
+           mapView.setRegion(region, animated: true)
+            
       }
 }
